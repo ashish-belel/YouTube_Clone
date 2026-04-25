4f82868af5d19e904df192d301bf2eb26bf7175d
BlackRavenMomon <ashishbelel70@gmail.com>
2026-04-25 22:22:49 +0530

    sorted all files

---
+++ scripts/data.js
@@ -0,0 +1,20 @@
+const videos = [
+  {
+    id: 1,
+    title: "Talking Tech and AI with Google CEO Sundar Pichai!",
+    author: "Marques Brownlee",
+    views: "3.4M views",
+    time: "14:20",
+    thumbnail: "assets/thumbnails/thumbnail-1.webp",
+    channelImg: "assets/images/channel-1.jpeg"
+  },
+  {
+    id: 2,
+    title: "Try Not To Laugh Challenge #9",
+    author: "Markiplier",
+    views: "19M views",
+    time: "8:22",
+    thumbnail: "assets/thumbnails/thumbnail-2.webp",
+    channelImg: "assets/images/channel-1.jpeg"
+  }
+];
---
+++ scripts/render.js
@@ -0,0 +1,30 @@
+function renderVideos(videoList) {
+  const container = document.getElementById("video-container");
+
+  container.innerHTML = "";
+
+  videoList.forEach(video => {
+    const videoHTML = `
+      <div class="v-preview">
+        <div class="thumb-row">
+          <img class="thumb" src="${video.thumbnail}">
+          <div class="v-time">${video.time}</div>
+        </div>
+
+        <div class="v-info-grid">
+          <div class="logo">
+            <img class="pfp" src="${video.channelImg}">
+          </div>
+
+          <div class="v-info">
+            <p class="v-title">${video.title}</p>
+            <p class="v-author">${video.author}</p>
+            <p class="v-stats">${video.views}</p>
+          </div>
+        </div>
+      </div>
+    `;
+
+    container.innerHTML += videoHTML;
+  });
+}
